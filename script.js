979b1e9bfb4a59d9060ffa88fb54d6002f5ec622
Cesc <83516572+cescps@users.noreply.github.com>
2024-03-27 22:49:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,16 @@
-let cet = document.querySelector('.cet .the-time');
-let edt = document.querySelector('.edt .the-time');
+const cet = document.querySelector('.cet .the-time');
 
-const apiKey = '4c5fdf5194263e3c8f02c66cd1ffe3fb';
+  const animate = () => {
+    const timeCET = moment().tz('Europe/Paris').format('HH:mm:ss');
+    cet.innerHTML = `<span style="color: ${document.body.classList.contains('dark-mode') ? 'white' : 'black'};">${timeCET}</span>`;
+  };
 
-const getWeatherData = async (city) => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
-  const data = await response.json();
-  return data;
-};
+  setInterval(animate, 1000);
+  animate();
 
-const animate = async () => {
-  const cetWeatherData = await getWeatherData('Sant Cugat del Vallès');
-  const edtWeatherData = await getWeatherData('Albany');
-
-  render(cet, 'Europe/Paris', cetWeatherData);
-  render(edt, 'America/New_York', edtWeatherData);
-};
-
-const render = (el, tmzn, weatherData) => {
-  const time = moment.tz(tmzn).format('HH:mm:ss');
-  
-
-  el.innerHTML = `${time}`;
-  
-  // Create a new div for weather info
-  const weatherInfoDiv = document.createElement('div');
-  weatherInfoDiv.innerHTML = `<img class="weather-icon" src="http://openweathermap.org/img/w/${icon}.png" alt="weather icon"> ${temperature}°C`;
-  
-  // Replace existing weather info or append new one
-  if (el.nextElementSibling) {
-    el.nextElementSibling.replaceWith(weatherInfoDiv);
-  } else {
-    el.parentElement.appendChild(weatherInfoDiv);
+  function toggleMode() {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+    animate();
   }
-};
-
-setInterval(animate, 1000);
-animate();
